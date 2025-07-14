fdb12d62f4dd2c23c7ca7516e9eb50c912c9e3a0
MIKGUI <166784567+MIKGUI@users.noreply.github.com>
2025-07-13 21:28:35 -0600

    m1.2

---
+++ Mente Mental/Assets/Script/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform personaje;
+    public Vector3 offset; // Puedes ajustar esto para modificar la distancia entre el personaje y la cámara
+
+    void Update()
+    {
+        if (personaje != null)
+        {
+            transform.position = personaje.position + offset;
+        }
+    }
+}
---
+++ Mente Mental/Assets/Script/Jugador.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    private Rigidbody2D rbd2d;
+    private Vector2 inputs2d;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbd2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputs2d.x = Input.GetAxisRaw("Horizontal");
+        inputs2d.y = Input.GetAxisRaw("Vertical");
+
+        inputs2d = inputs2d.normalized;
+
+        animator.SetFloat("Horizontal", inputs2d.x);
+        animator.SetFloat("Vertical", inputs2d.y);
+        animator.SetFloat("Speed", inputs2d.magnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        rbd2d.velocity = inputs2d * speed; // Corregido de linearVelocity a velocity
+    }
+}
